08fda70b8c4f61bb235fe9b0ebe1b29c0b4b5b44
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-08-31 00:03:54 -0500

    Initialize a new project

---
+++ webpack.config.babel.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                loaders: ['react-hot', 'babel-loader'],
+                test: /\.js$/,
+                include: [
+                    path.join(__dirname, 'src')
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+};
