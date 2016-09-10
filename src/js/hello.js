de2a2e70ba8be30f966fd6a0f150b09b66be3c41
Leonhardt Koepsell <leonhardt@koepsell.io>
2016-09-10 15:33:39 -0700

    Add personal links

--- src/js/hello.js
+++ src/js/hello.js
@@ -8,6 +8,14 @@ class Hello extends React.Component {
                     <div className="callout text-center">
                         <h1>Leonhardt Koepsell</h1>
                         <p><em>Software Engineer</em></p>
+                        <ul className="no-bullet">
+                            <li>medium: <a href="https://medium.com/@lnhrdt" target="_blank">@lnhrdt</a></li>
+                            <li>github: <a href="https://github.com/lnhrdt" target="_blank">@lnhrdt</a></li>
+                            <li>twitter: <a href="https://twitter.com/lnhrdt" target="_blank">@lnhrdt</a></li>
+                            <li>linkedin: <a href="https://www.linkedin.com/in/leonhardtkoepsell" target="_blank">leonhardtkoepsell</a></li>
+                            <li>email: <a href="mailto:leonhardt@koepsell.io">leonhardt@koepsell.io</a></li>
+                            <li>mobile: <a href="tel:+18182136258">+18182136258</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
